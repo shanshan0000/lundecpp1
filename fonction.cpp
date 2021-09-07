fcd654dbafcc6df47186e3eba8ee6d903660c3c6
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-07 17:17:43 +0800

    ex5-ans

--- fonction.cpp
+++ fonction.cpp
@@ -1,15 +1,19 @@
-#include<stdio.h>
-#define PI 3.14159
+#include <iostream>
+
+using namespace std;
 
 void exerciceA(){
-    int r; double p, s;
-    printf("donnez le rayon entier d’un cercle:");
-    scanf("%d",&r);
-    p=2*PI*r;
-    s=PI*r*r;
-    printf("le cercle de rayon %d ",r);
-    printf("a un perimetre de %f et une surface de %f\n",p,s);
+    const double PI=3.14159; // doit être initialisée
+    cout<<"donnez le rayon entier d’un cercle:";
+    int r;
+    cin>>r;
+    auto p=2*PI*r;
+    auto s=PI*r*r;
+    cout<<"le cercle de rayon "<<r<<" ";
+    cout<<"a un perimetre de "<<p
+        <<" et une surface de "<<s<<"\n";
 }
+
 
 int main() {
     exerciceA();
