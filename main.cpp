f6e3e35fca5d8772c555a6a673eeed5d7e82254b
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-07 18:12:45 +0800

    ex10

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double* pt0=0;
+    double* pt1=4096;
+    double* pt2=(double*)4096;
+    void* pt3=pt1;
+    pt1=pt3;
+    pt1=(double*)pt3;
+    double d1=36;
+    const double d2=36;
+    double* pt4=&d1;
+    const double* pt5=&d1;
+    *pt4=2.1;
+    *pt5=2.1;
+    pt4=&d2;
+    pt5=&d2;
+    double* const pt6=&d1;
+    pt6=&d1;
+    *pt6=2.78;
+    double* const pt6b=&d2;
+    const double* const pt7=&d1;
+    pt7=&d1;
+    *pt7=2.78;
+    double const* pt8=&d1;
+    pt8=&d2;
+    pt8=&d1;
+    *pt8=3.14;
     return 0;
 }
