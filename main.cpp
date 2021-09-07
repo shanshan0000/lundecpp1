e85cbbeb8c87c8b0a46b7f8e174beff215645ad0
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-07 22:34:23 +0800

    learn cout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout.operator<<(10).operator<<("Hello world").operator<<('c').operator<<(std::endl);
+    std::cout.operator<<("10").operator<<("Hello world").operator<<('c');
     return 0;
 }
