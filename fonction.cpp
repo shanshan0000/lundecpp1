9011b2f62aa9d6a77bb51b7341578c1593fe58e8
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-07 18:24:49 +0800

    ex9-ans-2

--- fonction.cpp
+++ fonction.cpp
@@ -1,10 +1,5 @@
 #include "fonction.h"
 
-int fct(int x){ std::cout<<"1:"<<x<<"\n"; return 0; }
-int fct(float y){ std::cout<<"2:"<<y<<"\n"; return 0; }
-int fct(int x, float y){ std::cout<<"3:"<<x<<y<<"\n"; return 0; }
-float fct(float x, int y){ std::cout<<"4:"<<x<<y<<"\n"; return 3.14; }
-
 void exercice_surcharge(){
     int i = 3, j = 15;
     float x = 3.14159, y = 1.414;
--- fonction.h
+++ fonction.h
@@ -3,10 +3,9 @@
 
 #include <iostream>
 
-int fct(int x);
-int fct(float y);
-int fct(int x, float y);
-float fct(float x, int y);
-
+inline int fct(int x){ std::cout<<"1:"<<x<<"\n"; return 0; }
+inline int fct(float y){ std::cout<<"2:"<<y<<"\n"; return 0; }
+inline int fct(int x, float y){ std::cout<<"3:"<<x<<" "<<y<<"\n"; return 0; }
+inline float fct(float x, int y){ std::cout<<"4:"<<x<<" "<<y<<"\n"; return float(3.14); }
 
 #endif //CPP_2021_FONCTION_H
