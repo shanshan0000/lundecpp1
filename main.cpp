b10beea137cc1fb0a50540548119211cf9ca7ccc
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-08 14:45:50 +0800

    ex11

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double& d1=36;
+    double d2=36;
+    double& ref=d2;
+    ref=4;
+    const double d3=36;
+    const double& d4=36;
+    const double& d5=d2;
+    d5=21;
+    const double& d6=d3;
+    double& ref2=d6;
+    int i=4;
+    double& d7=i;
+    const double& d8=i;
+    d8=3;
+
     return 0;
 }
