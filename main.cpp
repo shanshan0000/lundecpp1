e1821417ffd9140ca489c5817360103da7458d2d
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-08 14:57:11 +0800

    ex12 ans

--- main.cpp
+++ main.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 
+void inverse(int* a, int* b){
+    int tmp=*a;
+    *a=*b;
+    *b=tmp;
+}
+void inverse(int& a, int& b){
+    int tmp=a;
+    a=b;
+    b=tmp;
+}
+
+/*
+ *  void inverse(int a, int b) {
+// ne fonctionne pas, a et b sont des variables locales
+    auto tmp = a; a = b; b = tmp;
+}
+ */
+
+
 int main() {
-    //double& d1 = 36; // erreur, 36 n’est une lvalue
-    const double& d1 = 36; /* ok pour créer une reférence const
-                            un espace mémoire est créé temporairement
-                            pour stocker 36. C’est c’est cet espace mémoire
-                            qui est référencé.
-                            */
-    double d2 = 36;
-    double& ref = d2;
-    ref = 4; // équivalent à d2=4;
-    const double d3 = 36;
-    const double& d4 = 36;
-    const double& d5 = d2; /* on peut créer une ref const sur une donnée
-                            aussi bien constante que non constante */
-    //d5 = 21;
-                            /* erreur, avec une référence const, on ne peut pas modifier la donnée
-                            référencée, même si cette donnée n’est pas constante */
-    const double& d6 = d3;
-    //double& ref2 = d6; // conversion implicite const double&->double&
-    double& ref2 = const_cast<double&>(d6); // conversion explicite ok
-    int i = 4;
-    //double& d7 = i; // erreur, une T& doit initialisée avec une lvalue de type T
-    const double& d8 = i; /* les conversions sont possibles avec les ref const.
-                            Une donnée temporaire contenant le résultat de la
-                            conversion de i en double est créée.
-                            C’est cette donnée qui est référencée. La durée de
-                            vie de la donnée temporaire est la même que celle
-                            de la référence
-                            */
-    //d8 = 3; // erreur, on ne peut pas modifier la donnée référencée par une ref const
+    int x = 7, y = 9;
+    inverse(&x, &y);
+    inverse(x, y);
 
     return 0;
 }
