c3d188133e0b17446acac0a21e12344e9b37b0be
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-08 18:03:51 +0800

    ex16-ans

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include<array>
+
 using namespace std;
 
-int calcul(int x) { return 2 * x + 1; }
+constexpr int calcul(int x) { return 2 * x + 1; }
 
-int getNumber() { return 3; }
+constexpr int getNumber() { return 3; }
 
 int main() {
-    const int N = getNumber();
-    array<int, calcul(N)> tableau;
+    constexpr const int N = getNumber();
+    array<int,calcul(N)> tableau;
     return 0;
 }
