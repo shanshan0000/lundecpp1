e7043e5e7665114d156c76444b812c8146f19481
lunde chen <lunde.chen@yahoo.fr>
2021-09-08 09:03:56 +0800

    ex5-extended

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const auto PI = 3.14159;
+    int r;
+    double p, s;
+    std::cout << "donnez le rayon entier d’un cercle:" << std::endl << 10 << 100;
+    std::cout.operator<<(8).operator<<(44).operator<<(66);
     return 0;
 }
