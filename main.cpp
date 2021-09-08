81826839d4d3b2f8f703a4071f1ae03c45418175
lunde chen <lunde.chen@yahoo.fr>
2021-09-08 09:19:49 +0800

    ex5 extended

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 int main() {
     const auto PI = 3.14159;
     int r;
-    double p, s;
     std::cout << "donnez le rayon entier d’un cercle:" << std::endl << 10 << 100;
     std::cout.operator<<(8).operator<<(44).operator<<(66);
     std::cin >> r;
-    p=2*PI*r;
-    s=PI*r*r;
+    auto p=2*PI*r;
+    auto s=PI*r*r;
+    s = p + 9;
     std::cout << "le cercle de rayon " << r;
     return 0;
 }
