baf310f7d884bfc25e130b4fb22f0a35ce2706ce
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-08 16:01:25 +0800

    ex15

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+void essai_alloc(){
+    int* pt_int;
+    double* pt_double;
+    pt_int=(int*)malloc(sizeof(int));
+    pt_double=(double*)malloc(sizeof(double)*100);
+    free(pt_int);
+    free(pt_double);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    essai_alloc();
     return 0;
 }
