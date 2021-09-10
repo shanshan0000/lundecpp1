4acae56ea183f546f5d7b9e9a2493035f028194f
lunde chen <lunde.chen@yahoo.fr>
2021-09-10 11:14:26 +0800

    extended

---
+++ fonction.h
@@ -0,0 +1,12 @@
+#ifndef CPP_2021_FRIDAY_H
+#define CPP_2021_FRIDAY_H
+
+#include <iostream>
+
+int fct(int x);
+int fct(float y);
+int fct(int x, float y);
+float fct(float x, int y);
+
+
+#endif //CPP_2021_FRIDAY_H
