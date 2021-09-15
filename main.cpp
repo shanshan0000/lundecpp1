6665eceb319edeefd56e8c0041a3c96119c80d3b
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-15 19:41:53 +0800

    ex22-q5-ans

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 int main(){
     using namespace MATH;
     try{
-        Fraction A(1,2);
-        Fraction B(3,0);
+        Fraction A(1,2); Fraction B(3,0);
     }
-    catch(const char* e){ std::cout<<e<<"\n"; }
+    catch(FractionException& e){ std::cout<<e.getInfo()<<"\n"; }
 
 }
