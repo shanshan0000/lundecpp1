c9469364966ea83a69a30064d6be02304c595866
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-17 21:29:01 +0800

    ex27-q4-ans

--- evenement.h
+++ evenement.h
@@ -11,7 +11,12 @@ namespace TIME{
         Date date;
         std::string sujet;
     public:
-        Evt1j(const Date& d, const std::string& s):date(d),sujet(s){}
+        Evt1j(const Date& d, const std::string& s):date(d),sujet(s)
+        {
+            std::cout<<"construction d'un objet Evt1j\n";
+        }
+        ~Evt1j() { std::cout<<"destruction d'un objet Evt1j\n"; }
+
         const std::string& getDescription() const { return sujet; }
         const Date& getDate() const { return date; }
         void afficher(std::ostream& f= std::cout) const {
@@ -27,7 +32,12 @@ namespace TIME{
         Evt1jDur(const Date& d, const std::string& s,
                  const Horaire& h, const Duree t):
                 Evt1j(d,s), // appel au constructeur de la classe de base
-                debut(h),duree(t){}
+                debut(h),duree(t)
+        {
+            std::cout<<"construction d'un objet Evt1jDur\n";
+        }
+        ~Evt1jDur() { std::cout<<"destruction d'un objet Evt1jDur\n"; }
+
         const Horaire& getHoraire() const { return debut; }
         const Duree& getDuree() const { return duree; }
         void afficher(std::ostream& f= std::cout) const {
@@ -45,7 +55,12 @@ namespace TIME{
             const Horaire& h,const Duree& dur,
             const std::string& p, const std::string& l):
                 Evt1jDur(d,s,h,dur),
-                personne(p),lieu(l){}
+                personne(p),lieu(l)
+        {
+            std::cout<<"construction d'un objet Rdv\n";
+        }
+        ~Rdv() { std::cout<<"destruction d'un objet Rdv\n"; }
+
         const std::string& getPersonne() const { return personne; }
         const std::string& getLieu() const { return lieu; }
         void afficher(std::ostream& f=std::cout) const {
