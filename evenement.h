abdacbc1fe0aa6699e7d8461c722e905a04bd1f3
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-17 17:48:01 +0800

    ex27-DEV

--- evenement.h
+++ evenement.h
@@ -1,14 +1,24 @@
 #ifndef CPP_2021_EVENEMENT_H
 #define CPP_2021_EVENEMENT_H
 
+#include <iostream>
+#include <string>
+#include "timing.h"
 
-
-
-
-
-
-
-
+namespace TIME{
+    class Evt1j {
+    private:
+        Date date;
+        std::string sujet;
+    public:
+        Evt1j(const Date& d, const std::string& s):date(d),sujet(s){}
+        const std::string& getDescription() const { return sujet; }
+        const Date& getDate() const { return date; }
+        void afficher(std::ostream& f= std::cout) const {
+            f<<"***** Evt ********"<<"\n"<<"Date="<<date<<" sujet="<<sujet<<"\n";
+        }
+    };
+}
 
 
 #endif //CPP_2021_EVENEMENT_H
---
+++ timing.h
@@ -0,0 +1,81 @@
+#ifndef CPP_2021_TIMING_H
+#define CPP_2021_TIMING_H
+
+#include<string>
+#include<cstring>
+
+using  namespace  std;
+
+namespace TIME
+{
+    class Date
+    {
+    public:
+        int year, month, day;
+        Date(int y, int m, int d): year(y), month(m), day(d)
+        {}
+
+        string toString() const
+        {
+            return std::to_string(year)
+                   + " "
+                   + std::to_string(month)
+                   + " "
+                   + std::to_string(day);
+        }
+    };
+
+    class Duree
+    {
+    public:
+        int duree;
+        Duree(int d): duree(d){}
+
+        string toString() const
+        {
+            return std::to_string(duree);
+        }
+    };
+
+    class Horaire
+    {
+    public:
+        int hour, minute;
+        Horaire(int h, int m):hour(h), minute(m){}
+
+        string toString() const
+        {
+            return std::to_string(hour)
+                   + " "
+                   + std::to_string(minute);
+        }
+    };
+
+    inline std::ostream & operator<<(std::ostream & F, const Date & d) {
+        string s = std::to_string(d.year)
+                   + " "
+                   + std::to_string(d.month)
+                   + " "
+                   + std::to_string(d.day);
+        F << s;
+        return F;
+    }
+
+    inline std::ostream & operator<<(std::ostream & F, const Duree & d) {
+        string s = std::to_string(d.duree);
+        F << s;
+        return F;
+    }
+
+    inline std::ostream & operator<<(std::ostream & F, const Horaire & h) {
+        string s = std::to_string(h.hour)
+                   + " "
+                   + std::to_string(h.minute);
+        F << s;
+        return F;
+    }
+
+}
+
+
+#endif //CPP_2021_TIMING_H
