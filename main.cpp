7d99980b2b3d0a12caeedf3dee5451a4e8f67f8f
lunde chen <lunde.chen@yahoo.fr>
2021-09-22 09:25:14 +0800

    m

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "fraction.h"
 
 int main(){
+    using namespace MATH;
+    Fraction f1;
+    f1.setFraction(5, 8);
+    int n = f1.getNumerateur();
+    int d = f1.getDenominateur();
 
 
 
