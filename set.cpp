4bacedd527000935b240b37c2b3c179913509bf0
Lunde CHEN <lunde.chen@yahoo.fr>
2021-09-22 19:06:57 +0800

    ex23-q3-q4-q5

--- set.cpp
+++ set.cpp
@@ -2,6 +2,20 @@
 
 namespace Set {
 
+    // https://stackoverflow.com/questions/261963/how-can-i-iterate-over-an-enum
+    Jeu::Jeu() {
+        size_t i = 0;
+        for (auto c : Couleurs)
+            for (auto v : Nombres)
+                for (auto f : Formes)
+                    for (auto r : Remplissages)
+                        cartes[i++] = new Carte(c, v, f, r);
+    }
+
+    Jeu::~Jeu() {
+        for (size_t i = 0; i < getNbCartes(); i++) delete cartes[i];
+    }
+
     ostream& operator<<(ostream& f, const Carte& c) {
         f << "(" << c.getNombre() << "," << c.getCouleur()<<"," <<c.getForme()<<","
           << c.getRemplissage() << ")";
--- set.h
+++ set.h
@@ -11,6 +11,11 @@ namespace Set {
     enum Forme { ovale, vague, losange };
     enum Remplissage { plein, hachure, vide };
 
+    static const Couleur Couleurs[] = { rouge, violet, vert };
+    static const Nombre Nombres[] = {un, deux, trois };
+    static const Forme Formes[] = { ovale, vague, losange };
+    static const Remplissage Remplissages[] = { plein, hachure, vide };
+
     class Carte {
     public:
         Carte(Couleur c, Nombre v, Forme f, Remplissage r) :couleur(c), nombre(v),
@@ -27,6 +32,26 @@ namespace Set {
         Nombre nombre;
         Forme forme;
         Remplissage remplissage;
+    };
+
+    class Jeu {
+    public:
+        Jeu();
+        ~Jeu();
+        Jeu(const Jeu& j) = delete;
+        Jeu& operator=(const Jeu& j) = delete;
+        size_t getNbCartes() const
+        {
+            return 81;
+        }
+        const Carte& getCarte(size_t i) const
+        {
+            if (i >= 81)
+                throw SetException("Carte invalide");
+            return *cartes[i];
+        }
+    private:
+        const Carte* cartes[81];
     };
 
     ostream& operator<<(ostream& f, const Carte& c);
