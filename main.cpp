6f205ab1695b38fa6c760656f519b86229a9f31e
lunde chen <lunde.chen@yahoo.fr>
2021-09-26 08:19:01 +0800

    ex21-q1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "fraction.h"
+#include <iostream>
 
 int main(){
     using namespace MATH;
     Fraction f1(3,4);
     Fraction f2(5);
     Fraction f4 = f1 + f2; // utilisation de la fonction
+
+    f1 ++;
+    ++ f2;
+    
+    std::cout << f1 << std::endl;
 }
