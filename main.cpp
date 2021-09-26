f31e8e616d1d53a749a809c22bdccfd9f1886d09
lunde chen <lunde.chen@yahoo.fr>
2021-09-26 08:36:58 +0800

    leetcode 1748

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+// https://leetcode.com/problems/sum-of-unique-elements/
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int sum=0;
+        for(auto val:nums) {
+            m[val]++;
+        }
+        for(auto val:m) {
+            if(val.second==1)
+                sum+=val.first;
+        }
+        return sum;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution solution;
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(2);
+    std::cout << solution.sumOfUnique(v) << std::endl;
     return 0;
 }
