d4df765828c344a5eebc511ee8f2a6dda76fe1b0
lunde chen <lunde.chen@yahoo.fr>
2021-09-28 08:39:54 +0800

    leetcode-1603

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+
+class ParkingSystemTwo {
+public:
+    int big = 0 , small = 0, medium = 0;
+    ParkingSystemTwo(int big, int medium, int small) {
+        this->big = big ;
+        this->medium = medium ;
+        this->small = small ;
+    }
+
+    bool addCar(int carType) {
+        if(carType == 1 && big != 0)  {big-- ; return true ;}
+        else if(carType == 2 && medium != 0)  {medium-- ; return true ;}
+        else if(carType == 3 && small != 0)  {small-- ; return true ;}
+
+        return false ;
+    }
+};
+
+
+class ParkingSystemTwo {
+public:
+    int big = 0 , small = 0, medium = 0;
+    ParkingSystemTwo(int big, int medium, int small) {
+        this->big = big ;
+        this->medium = medium ;
+        this->small = small ;
+    }
+
+    bool addCar(int carType) {
+        if(carType == 1 && big != 0)  {big-- ; return true ;}
+        else if(carType == 2 && medium != 0)  {medium-- ; return true ;}
+        else if(carType == 3 && small != 0)  {small-- ; return true ;}
+
+        return false ;
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
