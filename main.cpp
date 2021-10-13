145acabfaf7f67276b671dd39e442dd3805b8688
lunde chen <lunde.chen@yahoo.fr>
2021-10-13 10:07:34 +0800

    leetcode-284-leetcode-284-v1

--- main.cpp
+++ main.cpp
@@ -3,27 +3,12 @@
 
 using namespace std;
 
-class Iterator {
-    struct Data;
-    Data* data;
-public:
-    Iterator(const vector<int>& nums);
-    Iterator(const Iterator& iter);
-
-    // Returns the next element in the iteration.
-    int next();
-
-    // Returns true if the iteration has more elements.
-    bool hasNext() const;
-};
-
-
-class PeekingIterator : public Iterator {
+class PeekingIterator {
 public:
     vector<int> v;
     int i = 0;
 
-    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+    PeekingIterator(const vector<int>& nums) {
         v = nums;
     }
 
@@ -43,12 +28,11 @@ public:
 
 int main() {
     const vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    PeekingIterator* pi = new PeekingIterator(v);
 
-    PeekingIterator pi(v);
-
-    while(pi.hasNext()){
-        cout << "pop:  " << pi.next();
-        cout << "peek: " << pi.peek();
+    while(pi->hasNext()){
+        cout << "pop:  " << pi->next();
+        cout << "  peek: " << pi->peek();
         cout << endl;
     }
 
