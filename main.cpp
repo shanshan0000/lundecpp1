e4a04258a4c87dd3c25a3846a066982441590e23
lunde chen <lunde.chen@yahoo.fr>
2021-10-13 10:44:32 +0800

    leetcode-2leetcode-284-v2

--- main.cpp
+++ main.cpp
@@ -20,28 +20,38 @@ public:
 
 class PeekingIterator : public Iterator {
 public:
-    int nextVal;
-
+    int prev;
+    bool end;
     PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-        nextVal = Iterator::hasNext() ? Iterator::next() : NULL;
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+        this->prev = Iterator::next();
+        this->end = 0;
     }
 
+    // Returns the next element in the iteration without advancing the iterator.
     int peek() {
-        return nextVal;
+        return this->prev;
     }
 
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
     int next() {
-        int oldNext = nextVal;
-        nextVal = Iterator::hasNext() ? Iterator::next() : NULL;
-        return oldNext;
+        int ans = this->prev;
+        if(Iterator::hasNext() == 0) this->end = 1;
+        else this->prev = Iterator::next();
+        return ans;
     }
 
     bool hasNext() const {
-        return (nextVal != NULL);
+        return !(this->end);
     }
 };
 
 int main() {
+    // https://leetcode.com/problems/peeking-iterator/discuss/1498942/C%2B%2B%3A-Simple-Solution-using-Iterator-Interface
+
     const vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     PeekingIterator pi(v);
