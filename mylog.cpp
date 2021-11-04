a53dd89dfb6da9be7cb13562823c64ff43686757
Lunde CHEN <lunde.chen@yahoo.fr>
2021-11-04 13:55:34 +0800

    ex35-q2-ans-2

--- mylog.cpp
+++ mylog.cpp
@@ -3,10 +3,11 @@
 using namespace TIME;
 
 void MyLog::addEvt(const TIME::Date& d, const TIME::Horaire& h,const std::string& s){
-    evts << TIME::Evt1jDur(d,s,h,TIME::Duree(0));
+    TIME::Evt1jDur e(d,s,h,TIME::Duree(0));
+    evts << e;
 }
 void MyLog::displayLog(std::ostream& f) const{
-    for(Agenda::iterator it=evts.begin(); it!=evts.end(); ++it)
+    for(Agenda::const_iterator it=evts.begin(); it!=evts.end(); ++it)
         f << dynamic_cast<const TIME::Evt1jDur&>(*it).
                 getHoraire()<<":"<<(*it).getDescription()<<"\n";
 }
