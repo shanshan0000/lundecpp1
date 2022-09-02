2ee242ef97cfc37f06be8211d00bc1b2a7693cc8
Lunde Chen <lundechen@shu.edu.cn>
2022-09-02 18:19:01 +0800

    2022 master

--- fonction.cpp
+++ fonction.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-void exerciceA(){
-    const double PI=3.14159; // doit être initialisée
-    cout<<"donnez le rayon entier d’un cercle:";
+void exerciceA() {
+    const double PI = 3.14159; // doit être initialisée
+    cout << "donnez le rayon entier d’un cercle:";
     int r;
-    cin>>r;
-    auto p=2*PI*r;
-    auto s=PI*r*r;
-    cout<<"le cercle de rayon "<<r<<" ";
-    cout<<"a un perimetre de "<<p
-        <<" et une surface de "<<s<<"\n";
+    cin >> r;
+    auto p = 2 * PI * r;
+    auto s = PI * r * r;
+    cout << "le cercle de rayon " << r << " ";
+    cout << "a un perimetre de " << p
+         << " et une surface de " << s << "\n";
 }
 
 
