9559806c1b327f19b5136b525b469407687f4d37
Lunde Chen <lundechen@shu.edu.cn>
2022-09-02 18:20:00 +0800

    2022 master

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
     std::cout << "donnez le rayon entier d’un cercle:" << std::endl << 10 << 100;
     std::cout.operator<<(8).operator<<(44).operator<<(66);
     std::cin >> r;
-    auto p=2*PI*r;
-    auto s=PI*r*r;
+    auto p = 2 * PI * r;
+    auto s = PI * r * r;
     s = p + 9;
     std::cout << "le cercle de rayon " << r;
     return 0;
