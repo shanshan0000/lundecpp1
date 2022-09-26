15893d81ae644be9831bdcba8234ace03f8a7f09
Lunde Chen <lunde.chen@outlook.com>
2022-09-26 21:58:15 +0800

    cpp

--- fraction.cpp
+++ fraction.cpp
@@ -10,60 +10,62 @@ void MATH::Fraction::setFraction(int n, int d) {
 //    }
 //    simplification();
 
-    if (d==0) throw "erreur : denominateur nul";
-    numerateur=n; denominateur=d; simplification();
+    if (d == 0) throw "erreur : denominateur nul";
+    numerateur = n;
+    denominateur = d;
+    simplification();
 }
 
-MATH::Fraction::Fraction(int n, int d):numerateur(n),denominateur(d) {
+MATH::Fraction::Fraction(int n, int d) : numerateur(n), denominateur(d) {
     setFraction(n, d);
     simplification();
 }
 
-void MATH::Fraction::simplification(){
-    if (numerateur==0) {
-        denominateur=1;
+void MATH::Fraction::simplification() {
+    if (numerateur == 0) {
+        denominateur = 1;
         return;
     }
-    if (denominateur==0)
+    if (denominateur == 0)
         return;
-    auto a=numerateur, b=denominateur;
-    if (a<0) a=-a;
-    if (b<0) b=-b;
-    while(a!=b){
-        if (a>b)
-            a=a-b;
+    auto a = numerateur, b = denominateur;
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    while (a != b) {
+        if (a > b)
+            a = a - b;
         else
-            b=b-a;
+            b = b - a;
     }
-    numerateur/=a;
-    denominateur/=a;
-    if (denominateur<0) {
-        denominateur=-denominateur;
-        numerateur=-numerateur;
+    numerateur /= a;
+    denominateur /= a;
+    if (denominateur < 0) {
+        denominateur = -denominateur;
+        numerateur = -numerateur;
     }
 }
 
-const MATH::Fraction MATH::operator+(const MATH::Fraction& f1, const MATH::
-Fraction& f2){
-    return MATH::Fraction(f1.getNumerateur()*f2.getDenominateur()
-                          +f2.getNumerateur()*f1.getDenominateur(),
-                          f1.getDenominateur()*f2.getDenominateur());
+const MATH::Fraction MATH::operator+(const MATH::Fraction &f1, const MATH::
+Fraction &f2) {
+    return MATH::Fraction(f1.getNumerateur() * f2.getDenominateur()
+                          + f2.getNumerateur() * f1.getDenominateur(),
+                          f1.getDenominateur() * f2.getDenominateur());
 }
 
 
-MATH::Fraction& MATH::Fraction::operator++() {
-    setFraction(getNumerateur()+getDenominateur(),getDenominateur());
+MATH::Fraction &MATH::Fraction::operator++() {
+    setFraction(getNumerateur() + getDenominateur(), getDenominateur());
     return *this;
 }
 
-const MATH::Fraction MATH::Fraction::operator++(int){
+const MATH::Fraction MATH::Fraction::operator++(int) {
     Fraction f(getNumerateur(), getDenominateur()); // copie de la fraction
-    setFraction(getNumerateur()+getDenominateur(),getDenominateur());
+    setFraction(getNumerateur() + getDenominateur(), getDenominateur());
     return f;
 }
 
-std::ostream& operator<<(std::ostream& F, const MATH::Fraction& frac){
-    F<<frac.getNumerateur();
-    if (frac.getDenominateur()!=1) F<<"/"<<frac.getDenominateur();
+std::ostream &operator<<(std::ostream &F, const MATH::Fraction &frac) {
+    F << frac.getNumerateur();
+    if (frac.getDenominateur() != 1) F << "/" << frac.getDenominateur();
     return F;
 }
