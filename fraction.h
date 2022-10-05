8ea44ea67d9cfe97d4329274a386034412477e59
Lunde Chen <lunde.chen@outlook.com>
2022-10-05 20:17:38 +0800

    cpp

--- fraction.h
+++ fraction.h
@@ -1,5 +1,5 @@
-#ifndef CPP_2021_FRACTION_H
-#define CPP_2021_FRACTION_H
+#ifndef CPP_FRACTION_H
+#define CPP_FRACTION_H
 
 #include<iostream> // nécessaire pour utiliser les objets \cc{ostream}
 
@@ -31,4 +31,4 @@ namespace MATH {
 }
 
 
-#endif //CPP_2021_FRACTION_H
+#endif //CPP_FRACTION_H
