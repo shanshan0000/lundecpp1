522612a5504ce3231be62994958c390572a6fa6f
Lunde Chen <lunde.chen@outlook.com>
2022-10-05 19:55:32 +0800

    cpp

--- fonction.h
+++ fonction.h
@@ -1,5 +1,5 @@
-#ifndef CPP_2021_FONCTION_H
-#define CPP_2021_FONCTION_H
+#ifndef CPP_FONCTION_H
+#define CPP_FONCTION_H
 
 #include <iostream>
 
@@ -17,4 +17,4 @@ void copy_struct(personne& dest, const personne& source);
 void copy_tab(personne* dest, const personne* source, size_t n);
 
 
-#endif //CPP_2021_FONCTION_H
+#endif //CPP_FONCTION_H
