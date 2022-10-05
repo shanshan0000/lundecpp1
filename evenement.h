401418d17a47ff725526e8b2d22983357d5ceb0b
Lunde Chen <lunde.chen@outlook.com>
2022-10-05 22:50:52 +0800

    cpp

--- evenement.h
+++ evenement.h
@@ -1,5 +1,5 @@
-#ifndef CPP_2021_EVENEMENT_H
-#define CPP_2021_EVENEMENT_H
+#ifndef CPP_EVENEMENT_H
+#define CPP_EVENEMENT_H
 
 #include <iostream>
 #include <string>
@@ -56,4 +56,4 @@ namespace TIME{
 }
 
 
-#endif //CPP_2021_EVENEMENT_H
+#endif //CPP_EVENEMENT_H
