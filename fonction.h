99abdf08dbda7d988a5cd163f28540e194c765a2
Lunde Chen <lunde.chen@outlook.com>
2022-10-05 19:49:52 +0800

    cpp

--- fonction.h
+++ fonction.h
@@ -1,5 +1,5 @@
-#ifndef CPP_2021_FONCTION_H
-#define CPP_2021_FONCTION_H
+#ifndef CPP_FONCTION_H
+#define CPP_FONCTION_H
 
 #include <iostream>
 
@@ -8,4 +8,4 @@ inline int fct(float y){ std::cout<<"2:"<<y<<"\n"; return 0; }
 inline int fct(int x, float y){ std::cout<<"3:"<<x<<" "<<y<<"\n"; return 0; }
 inline float fct(float x, int y){ std::cout<<"4:"<<x<<" "<<y<<"\n"; return float(3.14); }
 
-#endif //CPP_2021_FONCTION_H
+#endif //CPP_FONCTION_H
