2c0f0b6097cc6079c3e22217ddb9978b06ea4d79
Lunde Chen <lunde.chen@outlook.com>
2022-10-18 11:04:58 +0800

    cpp

--- fonction.h
+++ fonction.h
@@ -1,14 +1,51 @@
 #ifndef CPP_FONCTION_H
 #define CPP_FONCTION_H
 
+// Reference: https://www.internalpointers.com/post/writing-custom-iterators-modern-cpp
 
+class Integers {
+public:
+    int m_data[200];
 
+    Integers()
+    {
+        for(int i = 0; i <= 20; i ++)
+            m_data[i] = i;
+    }
 
+    class Iterator {
+    public:
+        Iterator(int* ptr) : m_ptr(ptr) {}
 
+        int& operator*() const
+        {
+            return *m_ptr;
+        }
 
+        // Prefix increment
+        Iterator &operator++()
+        {
+            m_ptr++;
+            return *this;
+        }
 
+        // Postfix increment
+        Iterator operator++(int)
+        {
+            Iterator tmp = *this;
+            ++(*this);
+            return tmp;  // or, return Iterator(tmp);
+        }
 
+        bool operator!=(const Iterator &b)
+        {
+            return m_ptr != b.m_ptr;
+        }
 
+    private:
+        int* m_ptr;
+    };
+};
 
 
 #endif //CPP_FONCTION_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "fonction.h"
 
 int main() {
+    Integers a;
 
 
 
