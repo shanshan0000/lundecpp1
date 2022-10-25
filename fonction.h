e9561de8acc3077ed70788cb99ff5e2802099bdb
Lunde Chen <lunde.chen@outlook.com>
2022-10-25 12:04:26 +0800

    cpp

--- fonction.h
+++ fonction.h
@@ -1,8 +1,63 @@
 #ifndef CPP_FONCTION_H
 #define CPP_FONCTION_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Person {
+protected:
+    int sex;
+public:
+    string name;
+    void work(){
+        if(sex == 0)
+            cout << "996";
+        else
+            cout << "955";
+    }
+};
+
+class Computer {
+public:
+    string CPU;
+};
+
+class Robot : public Person, public Computer {
+};
 
 
+class Student : private Person {
+public:
+    string id;
+    void takeExam(){
+        if(sex == 0)
+            cout << name << "Online";
+        else
+            cout << "In place";
+    }
+
+    void work() {
+        cout << "read a lot of books"
+        << " in a ";
+        Person::work();
+        cout << " manner";
+    }
+};
+
+class UniversityStudent : public Student {
+public:
+    void goForInternship() {
+    }
+};
+
+
+class Scientist : public Person {
+public:
+    int IQ;
+    void writePaper() {}
+};
 
 
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "fonction.h"
 
 int main() {
+    Student s;
+    s.work();
 
-
+    Robot r;
+    r.name;
+    r.CPU;
 
     return 0;
 }
