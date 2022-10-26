49304faadd5a8b4bb996bc859f50aad7785e9f40
Lunde Chen <lunde.chen@outlook.com>
2022-10-26 11:26:02 +0800

    cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@
 int main() {
     using namespace std;
     using namespace TIME;
-    Evt1j e1(Date(4,10,1957),"Spoutnik");
-    Evt1j e2(Date(11,6,2013),"Shenzhou");
     Evt1jDur e3(Date(11,6,2013),"Lancement de Longue Marche",Horaire(17,38),
             Duree(0,10));
-    e1.afficher();
-    e2.afficher();
-    e3.afficher();
     return 0;
 }
