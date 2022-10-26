51a84111e74fdd8451ac1d12200727d6ee622369
Lunde Chen <lunde.chen@outlook.com>
2022-10-26 11:28:36 +0800

    cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 int main() {
     using namespace std;
     using namespace TIME;
-    Evt1jDur e3(Date(11,6,2013),"Lancement de Longue Marche",Horaire(17,38),Duree
-            (0,10));
-    Rdv e4(Date(11,4,2013),"reunion UV",Horaire(17,30),Duree(60),"Intervenants UV","bureau");
-    Evt1j* pt3=&e3; Evt1j* pt4=&e4;
-    pt3->afficher(); pt4->afficher();
+    Evt1jDur e3(Date(11,6,2013),"Lancement de Longue Marche",
+            Horaire(17,38),Duree(0,10));
+    Rdv e4(Date(11,4,2013),"reunion UV",
+            Horaire(17,30),Duree(60),
+            "Intervenants UV","bureau");
+    Evt1j* pt3=&e3;
+    Evt1j* pt4=&e4;
+    pt3->afficher();
+    pt4->afficher();
     return 0;
 }
