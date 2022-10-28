ec3296d766d73253a3a5bb2cd856256b93b5cfd3
Lunde Chen <lunde.chen@outlook.com>
2022-10-28 11:32:31 +0800

    cpp

--- evenement.cpp
+++ evenement.cpp
@@ -2,38 +2,42 @@
 
 using namespace TIME;
 
-std::ostream& operator<<(std::ostream& f, const Evt& e){
+std::ostream &operator<<(std::ostream &f, const Evt &e) {
     e.afficher(f);
     return f;
 }
 
-void EvtPj::afficher(std::ostream& f) const {
-    f<<"***** EvtPj ********"<<"\n";
+void EvtPj::afficher(std::ostream &f) const {
+    f << "***** EvtPj ********" << "\n";
 }
 
-void Evt1j::afficher(std::ostream& f) const {
-    f<<"***** Evt ********"<<"\n"<<"Date="<<date<<"\n";
+void Evt1j::afficher(std::ostream &f) const {
+    f << "***** Evt ********" << "\n" << "Date=" << date << "\n";
 }
 
 
-void Evt1jDur::afficher(std::ostream& f) const {
+void Evt1jDur::afficher(std::ostream &f) const {
     //f<<"***** Evt ********"<<"\n"<<"Date="<<date<<" sujet="<<sujet<<"\n";
     Evt1j::afficher(f); // rappel de la méthode de la classe de base.
-    f<<"debut="<<debut<<" duree="<<duree<<"\n";
+    f << "debut=" << debut << " duree=" << duree << "\n";
 }
 
-void Rdv::afficher(std::ostream& f) const {
+void Rdv::afficher(std::ostream &f) const {
     Evt1jDur::afficher(f); // rappel de la méthode de la classe de base.
-    f<<"personne(s)="<<personne<<" lieu="<<lieu<<"\n";
+    f << "personne(s)=" << personne << " lieu=" << lieu << "\n";
 }
 
-TIME::Agenda& TIME::Agenda::operator<<(TIME::Evt& e) {
+TIME::Agenda &TIME::Agenda::operator<<(TIME::Evt &e) {
     tab.push_back(&e);
     return *this;
 }
-void TIME::Agenda::afficher(std::ostream& f) const{
-    f<<"##### AGENDA #####"<<"\n";
-    for(unsigned int i=0; i<tab.size(); i++) { tab[i]->afficher(f); }
-    f<<"##### FIN AGENDA #####"<<"\n";
+
+void TIME::Agenda::afficher(std::ostream &f) const {
+    f << "##### AGENDA #####" << "\n";
+    for (unsigned int i = 0; i < tab.size(); i++) {
+        tab[i]->afficher(f);
+    }
+    f << "##### FIN AGENDA #####" << "\n";
 }
-TIME::Agenda::~Agenda(){ }
+
+TIME::Agenda::~Agenda() {}
--- evenement.h
+++ evenement.h
@@ -6,36 +6,42 @@
 #include <vector>
 #include "timing.h"
 
-namespace TIME{
+namespace TIME {
     class Evt {
     private:
         std::string sujet;
     public:
-        Evt(const std::string& s): sujet(s){}
-        const std::string& getDescription() const { return sujet; }
-        virtual void afficher(std::ostream& f=std::cout) const=0;
+        Evt(const std::string &s) : sujet(s) {}
+
+        const std::string &getDescription() const { return sujet; }
+
+        virtual void afficher(std::ostream &f = std::cout) const = 0;
     };
 
     class EvtPj : public Evt {
         Date debut;
         Date fin;
     public:
-        EvtPj(const Date& d,const Date& f, const std::string& s):
-                Evt(s),debut(d),fin(f){}
-        const Date& getDateDebut() const { return debut; }
-        const Date& getDateFin() const { return fin; }
-        void afficher(std::ostream& f=std::cout) const;
+        EvtPj(const Date &d, const Date &f, const std::string &s) :
+                Evt(s), debut(d), fin(f) {}
+
+        const Date &getDateDebut() const { return debut; }
+
+        const Date &getDateFin() const { return fin; }
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 
-    class Evt1j : public Evt{
+    class Evt1j : public Evt {
     private:
         Date date;
     public:
-        Evt1j(const Date& d, const std::string& s): Evt(s), date(d){}
-        const Date& getDate() const { return date; }
-        virtual void afficher(std::ostream& f= std::cout) const;
-    };
+        Evt1j(const Date &d, const std::string &s) : Evt(s), date(d) {}
 
+        const Date &getDate() const { return date; }
+
+        virtual void afficher(std::ostream &f = std::cout) const;
+    };
 
 
     class Evt1jDur : public Evt1j {
@@ -43,55 +49,63 @@ namespace TIME{
         Horaire debut;
         Duree duree;
     public:
-        Evt1jDur(const Date& d, const std::string& s,
-                 const Horaire& h, const Duree t):
-                Evt1j(d,s), // appel au constructeur de la classe de base
-                debut(h),duree(t){}
-        const Horaire& getHoraire() const { return debut; }
-        const Duree& getDuree() const { return duree; }
-        void afficher(std::ostream& f= std::cout) const;
+        Evt1jDur(const Date &d, const std::string &s,
+                 const Horaire &h, const Duree t) :
+                Evt1j(d, s), // appel au constructeur de la classe de base
+                debut(h), duree(t) {}
+
+        const Horaire &getHoraire() const { return debut; }
+
+        const Duree &getDuree() const { return duree; }
+
+        void afficher(std::ostream &f = std::cout) const;
 
     };
-
 
 
     class Rdv : public Evt1jDur {
         std::string personne;
         std::string lieu;
     public:
-        Rdv(const Date& d, const std::string& s,
-            const Horaire& h,const Duree& dur,
-            const std::string& p, const std::string& l):
-                Evt1jDur(d,s,h,dur),
-                personne(p),lieu(l){}
+        Rdv(const Date &d, const std::string &s,
+            const Horaire &h, const Duree &dur,
+            const std::string &p, const std::string &l) :
+                Evt1jDur(d, s, h, dur),
+                personne(p), lieu(l) {}
 
-        Rdv& operator=(const Rdv& r){
-            Evt1jDur* x=this;
-            *x=r; // recopie de la partie Evt1jDur
-            personne=r.personne;
-            lieu=r.lieu;
+        Rdv &operator=(const Rdv &r) {
+            Evt1jDur *x = this;
+            *x = r; // recopie de la partie Evt1jDur
+            personne = r.personne;
+            lieu = r.lieu;
             return *this;
         }
 
-        Rdv(const Rdv& r):Evt1jDur(r),personne(r.personne),lieu(r.lieu){}
+        Rdv(const Rdv &r) : Evt1jDur(r), personne(r.personne), lieu(r.lieu) {}
 
-        const std::string& getPersonne() const { return personne; }
-        const std::string& getLieu() const { return lieu; }
-        void afficher(std::ostream& f= std::cout) const;
+        const std::string &getPersonne() const { return personne; }
+
+        const std::string &getLieu() const { return lieu; }
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 
     class Agenda {
-        std::vector<Evt*> tab;
+        std::vector<Evt *> tab;
     public:
         Agenda() = default;
+
         virtual ~Agenda(); // destructeur par défaut virtuel
-        Agenda(const Agenda&) = delete;
-        Agenda& operator=(const Agenda&) = delete;
-        Agenda& operator<<(Evt& e);
-        void afficher(std::ostream& f = std::cout)const;
+        Agenda(const Agenda &) = delete;
+
+        Agenda &operator=(const Agenda &) = delete;
+
+        Agenda &operator<<(Evt &e);
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 }
 
-std::ostream& operator<<(std::ostream&, const TIME::Evt&);
+std::ostream &operator<<(std::ostream &, const TIME::Evt &);
 
 #endif //CPP_EVENEMENT_H
