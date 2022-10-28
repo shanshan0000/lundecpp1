9c7afac1dbb27628a69bf88cadd7a8fd2e7a5958
Lunde Chen <lunde.chen@outlook.com>
2022-10-28 09:58:10 +0800

    cpp

--- fonction.h
+++ fonction.h
@@ -3,28 +3,25 @@
 
 // Reference: https://www.internalpointers.com/post/writing-custom-iterators-modern-cpp
 
-class Item
-{
+class Item {
 public:
     int value;
 };
 
 class Integers {
 public:
-    Item** m_data;
+    Item **m_data;
 
-    Integers()
-    {
-        m_data = new Item*[200];
-        for(int i = 0; i <= 200; i ++)
-        {
+    Integers() {
+        m_data = new Item *[200];
+        for (int i = 0; i <= 200; i++) {
             m_data[i] = new Item;
             m_data[i]->value = i;
         }
     }
 
-    ~Integers(){
-        for(int i = 0; i <= 200; i ++)
+    ~Integers() {
+        for (int i = 0; i <= 200; i++)
             delete m_data[i];
         delete[] m_data;
     }
@@ -32,41 +29,38 @@ public:
 
     class Iterator {
     public:
-        Iterator(Item** ptr) : m_ptr(ptr) {}
+        Iterator(Item **ptr) : m_ptr(ptr) {}
 
-        Item& operator*() const
-        {
+        Item &operator*() const {
             return **m_ptr;
         }
 
-        Item* operator->() const { return *m_ptr; }
+        Item *operator->() const { return *m_ptr; }
 
         // Prefix increment
-        Iterator &operator++()
-        {
+        Iterator &operator++() {
             m_ptr++;
             return *this;
         }
 
         // Postfix increment
-        Iterator operator++(int)
-        {
+        Iterator operator++(int) {
             Iterator tmp = *this;
             ++(*this);
             return tmp;  // or, return Iterator(tmp);
         }
 
-        bool operator!=(const Iterator &b) const
-        {
+        bool operator!=(const Iterator &b) const {
             return m_ptr != b.m_ptr;
         }
 
     private:
-        Item** m_ptr;
+        Item **m_ptr;
     };
 
     Iterator begin() const { return Iterator(&(m_data[0])); }
-    Iterator end()   const { return Iterator(&(m_data[200])); } // 200 is out of bounds
+
+    Iterator end() const { return Iterator(&(m_data[200])); } // 200 is out of bounds
 };
 
 
