b988dc003775629621cd439dfbe204419610e55b
Lunde Chen <lunde.chen@outlook.com>
2022-11-02 10:22:51 +0800

    cpp

--- evenement.h
+++ evenement.h
@@ -6,36 +6,42 @@
 #include <vector>
 #include "timing.h"
 
-namespace TIME{
+namespace TIME {
     class Evt {
     private:
         std::string sujet;
     public:
-        Evt(const std::string& s): sujet(s){}
-        const std::string& getDescription() const { return sujet; }
-        virtual void afficher(std::ostream& f=std::cout) const=0;
+        Evt(const std::string &s) : sujet(s) {}
+
+        const std::string &getDescription() const { return sujet; }
+
+        virtual void afficher(std::ostream &f = std::cout) const = 0;
     };
 
     class EvtPj : public Evt {
         Date debut;
         Date fin;
     public:
-        EvtPj(const Date& d,const Date& f, const std::string& s):
-                Evt(s),debut(d),fin(f){}
-        const Date& getDateDebut() const { return debut; }
-        const Date& getDateFin() const { return fin; }
-        void afficher(std::ostream& f=std::cout) const;
+        EvtPj(const Date &d, const Date &f, const std::string &s) :
+                Evt(s), debut(d), fin(f) {}
+
+        const Date &getDateDebut() const { return debut; }
+
+        const Date &getDateFin() const { return fin; }
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 
-    class Evt1j : public Evt{
+    class Evt1j : public Evt {
     private:
         Date date;
     public:
-        Evt1j(const Date& d, const std::string& s): Evt(s), date(d){}
-        const Date& getDate() const { return date; }
-        virtual void afficher(std::ostream& f= std::cout) const;
-    };
+        Evt1j(const Date &d, const std::string &s) : Evt(s), date(d) {}
 
+        const Date &getDate() const { return date; }
+
+        virtual void afficher(std::ostream &f = std::cout) const;
+    };
 
 
     class Evt1jDur : public Evt1j {
@@ -43,113 +49,141 @@ namespace TIME{
         Horaire debut;
         Duree duree;
     public:
-        Evt1jDur(const Date& d, const std::string& s,
-                 const Horaire& h, const Duree t):
-                Evt1j(d,s), // appel au constructeur de la classe de base
-                debut(h),duree(t){}
-        const Horaire& getHoraire() const { return debut; }
-        const Duree& getDuree() const { return duree; }
-        void afficher(std::ostream& f= std::cout) const;
+        Evt1jDur(const Date &d, const std::string &s,
+                 const Horaire &h, const Duree t) :
+                Evt1j(d, s), // appel au constructeur de la classe de base
+                debut(h), duree(t) {}
+
+        const Horaire &getHoraire() const { return debut; }
+
+        const Duree &getDuree() const { return duree; }
+
+        void afficher(std::ostream &f = std::cout) const;
 
     };
-
 
 
     class Rdv : public Evt1jDur {
         std::string personne;
         std::string lieu;
     public:
-        Rdv(const Date& d, const std::string& s,
-            const Horaire& h,const Duree& dur,
-            const std::string& p, const std::string& l):
-                Evt1jDur(d,s,h,dur),
-                personne(p),lieu(l){}
+        Rdv(const Date &d, const std::string &s,
+            const Horaire &h, const Duree &dur,
+            const std::string &p, const std::string &l) :
+                Evt1jDur(d, s, h, dur),
+                personne(p), lieu(l) {}
 
-        Rdv& operator=(const Rdv& r){
-            Evt1jDur* x=this;
-            *x=r; // recopie de la partie Evt1jDur
-            personne=r.personne;
-            lieu=r.lieu;
+        Rdv &operator=(const Rdv &r) {
+            Evt1jDur *x = this;
+            *x = r; // recopie de la partie Evt1jDur
+            personne = r.personne;
+            lieu = r.lieu;
             return *this;
         }
 
-        Rdv(const Rdv& r):Evt1jDur(r),personne(r.personne),lieu(r.lieu){}
+        Rdv(const Rdv &r) : Evt1jDur(r), personne(r.personne), lieu(r.lieu) {}
 
-        const std::string& getPersonne() const { return personne; }
-        const std::string& getLieu() const { return lieu; }
-        void afficher(std::ostream& f= std::cout) const;
+        const std::string &getPersonne() const { return personne; }
+
+        const std::string &getLieu() const { return lieu; }
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 
     class Agenda {
-        std::vector<Evt*> tab;
+        std::vector<Evt *> tab;
     public:
-        class iterator : public std::vector<Evt*>::iterator {
+        class iterator : public std::vector<Evt *>::iterator {
         public:
-            Evt & operator*() const {
-                return *std::vector<Evt*>::iterator::operator*();
+            Evt &operator*() const {
+                return *std::vector<Evt *>::iterator::operator*();
             }
+
         private:
             friend class Agenda;
-            iterator(const std::vector<Evt*>::iterator& it):
-                    std::vector<Evt*>::iterator(it) {}
+
+            iterator(const std::vector<Evt *>::iterator &it) :
+                    std::vector<Evt *>::iterator(it) {}
         };
+
         iterator begin() { return iterator(tab.begin()); }
+
         iterator end() { return iterator(tab.end()); }
 
-        class const_iterator : public std::vector<Evt*>::const_iterator {
+        class const_iterator : public std::vector<Evt *>::const_iterator {
         public:
-            const Evt& operator*() const {
-                return *std::vector<Evt*>::const_iterator::operator*();
+            const Evt &operator*() const {
+                return *std::vector<Evt *>::const_iterator::operator*();
             }
+
         private:
             friend class Agenda;
-            const_iterator(const std::vector<Evt*>::const_iterator& it) :
-                    std::vector<Evt*>::const_iterator(it) {}
+
+            const_iterator(const std::vector<Evt *>::const_iterator &it) :
+                    std::vector<Evt *>::const_iterator(it) {}
         };
+
         const_iterator cbegin() const { return const_iterator(tab.cbegin()); }
+
         const_iterator cend() const { return const_iterator(tab.cend()); }
+
         const_iterator begin() const { return const_iterator(tab.begin()); }
+
         const_iterator end() const { return const_iterator(tab.end()); }
 
 
-        class reverse_iterator : public std::vector<Evt*>::reverse_iterator  {
+        class reverse_iterator : public std::vector<Evt *>::reverse_iterator {
         public:
-            Evt & operator*() const {
-                return *std::vector<Evt*>::reverse_iterator ::operator*();
+            Evt &operator*() const {
+                return *std::vector<Evt *>::reverse_iterator::operator*();
             }
-        private:
-            friend class Agenda;
-            reverse_iterator (const std::vector<Evt*>::reverse_iterator & it):
-                    std::vector<Evt*>::reverse_iterator (it) {}
-        };
-        reverse_iterator  rbegin() { return reverse_iterator (tab.rbegin()); }
-        reverse_iterator  rend() { return reverse_iterator (tab.rend()); }
 
-        class const_reverse_iterator : public std::vector<Evt*>::const_reverse_iterator  {
-        public:
-            const Evt & operator*() const {
-                return *std::vector<Evt*>::const_reverse_iterator ::operator*();
-            }
         private:
             friend class Agenda;
-            const_reverse_iterator (const std::vector<Evt*>::const_reverse_iterator & it):
-                    std::vector<Evt*>::const_reverse_iterator (it) {}
+
+            reverse_iterator(const std::vector<Evt *>::reverse_iterator &it) :
+                    std::vector<Evt *>::reverse_iterator(it) {}
         };
-        const_reverse_iterator  crbegin() const { return const_reverse_iterator (tab.crbegin()); }
-        const_reverse_iterator  crend() const { return const_reverse_iterator (tab.crend()); }
-        const_reverse_iterator  rbegin() const { return const_reverse_iterator (tab.rbegin()); }
-        const_reverse_iterator  rend() const { return const_reverse_iterator (tab.rend()); }
+
+        reverse_iterator rbegin() { return reverse_iterator(tab.rbegin()); }
+
+        reverse_iterator rend() { return reverse_iterator(tab.rend()); }
+
+        class const_reverse_iterator : public std::vector<Evt *>::const_reverse_iterator {
+        public:
+            const Evt &operator*() const {
+                return *std::vector<Evt *>::const_reverse_iterator::operator*();
+            }
+
+        private:
+            friend class Agenda;
+
+            const_reverse_iterator(const std::vector<Evt *>::const_reverse_iterator &it) :
+                    std::vector<Evt *>::const_reverse_iterator(it) {}
+        };
+
+        const_reverse_iterator crbegin() const { return const_reverse_iterator(tab.crbegin()); }
+
+        const_reverse_iterator crend() const { return const_reverse_iterator(tab.crend()); }
+
+        const_reverse_iterator rbegin() const { return const_reverse_iterator(tab.rbegin()); }
+
+        const_reverse_iterator rend() const { return const_reverse_iterator(tab.rend()); }
 
 
         Agenda() = default;
+
         virtual ~Agenda(); // destructeur par défaut virtuel
-        Agenda(const Agenda&) = delete;
-        Agenda& operator=(const Agenda&) = delete;
-        Agenda& operator<<(Evt& e);
-        void afficher(std::ostream& f = std::cout) const;
+        Agenda(const Agenda &) = delete;
+
+        Agenda &operator=(const Agenda &) = delete;
+
+        Agenda &operator<<(Evt &e);
+
+        void afficher(std::ostream &f = std::cout) const;
     };
 }
 
-std::ostream& operator<<(std::ostream&, const TIME::Evt&);
+std::ostream &operator<<(std::ostream &, const TIME::Evt &);
 
 #endif //CPP_EVENEMENT_H
--- main.cpp
+++ main.cpp
@@ -2,35 +2,38 @@
 #include "evenement.h"
 
 
-class MyVector{
+class MyVector {
 public:
-    int* arr;
-    MyVector()
-    {
+    int *arr;
+
+    MyVector() {
         arr = new int[5];
-        for(int i = 0; i <= 4; i ++)
+        for (int i = 0; i <= 4; i++)
             arr[i] = i;
     }
-    class iterator{
+
+    class iterator {
     public:
-        int* current;
-        iterator(int* c) : current(c){}
-        bool operator!=(iterator another){
+        int *current;
+
+        iterator(int *c) : current(c) {}
+
+        bool operator!=(iterator another) {
             return current != another.current;
         }
 
-        iterator& operator++(){
-            current ++;
+        iterator &operator++() {
+            current++;
             return *this;
         }
 
-        iterator operator++(int){
-            int* tmp = current;
-            current ++;
+        iterator operator++(int) {
+            int *tmp = current;
+            current++;
             return iterator(tmp);
         }
 
-        const int operator*() const{
+        const int operator*() const {
             return *current;
         }
 
@@ -45,41 +48,42 @@ public:
     }
 };
 
-class MyAgenda
-{
+class MyAgenda {
 public:
     MyVector tab;
-    MyAgenda(){
+
+    MyAgenda() {
         tab = MyVector();
     }
-    class iterator : public MyVector::iterator{
+
+    class iterator : public MyVector::iterator {
     public:
         // 这儿的星号可以不重载
-        int operator*(){
+        int operator*() {
             return MyVector::iterator::operator*();
         }
 
-        iterator(const MyVector::iterator& it): MyVector::iterator(it){};
+        iterator(const MyVector::iterator &it) : MyVector::iterator(it) {};
     };
 
-    iterator begin() const{
+    iterator begin() const {
         return iterator(tab.begin());
     }
 
-    iterator end() const{
+    iterator end() const {
         return iterator(tab.end());
     }
 
-    class iterator2 : public MyVector::iterator{
+    class iterator2 : public MyVector::iterator {
     public:
-        iterator2(int* cc) : MyVector::iterator(cc){}
+        iterator2(int *cc) : MyVector::iterator(cc) {}
     };
 
-    iterator2 begin2() const{
+    iterator2 begin2() const {
         return iterator2(tab.begin().current);
     }
 
-    iterator2 end2() const{
+    iterator2 end2() const {
         return iterator2(tab.end().current);
     }
 };
@@ -88,32 +92,34 @@ public:
 int main() {
     using namespace std;
     using namespace TIME;
-    Evt1j e1(Date(4,10,1957),"Spoutnik");
-    Evt1jDur e2(Date(4,4,2012),"courses",Horaire(19,0),Duree(45));
-    Rdv e3(Date(11,4,2012),"reunion UV",Horaire(17,30),Duree(60),"Intervenants UV","bureau");
-    Evt1j e4(Date(20,7,1969),"Premier Homme sur la Lune");
-    Evt1j e5(Date(14,7,1789),"Prise de la Bastille");
-    Rdv e6(Date(9,4,2012),"reunion",Horaire(10,45),Duree(1,30),"etudiant","passerelle");
-    Evt1jDur e7(Date(6,12,2012),"St Nicolas",Horaire(19,0),Duree(45));
-    EvtPj e8(Date(21,6,2010), Date(28,6,2010), "examens");
-    Rdv e9(Date(19,5,2013),"rdv",Horaire(11,45),Duree(2,30),"Mr X","Utseus");
+    Evt1j e1(Date(4, 10, 1957), "Spoutnik");
+    Evt1jDur e2(Date(4, 4, 2012), "courses", Horaire(19, 0), Duree(45));
+    Rdv e3(Date(11, 4, 2012), "reunion UV", Horaire(17, 30), Duree(60), "Intervenants UV", "bureau");
+    Evt1j e4(Date(20, 7, 1969), "Premier Homme sur la Lune");
+    Evt1j e5(Date(14, 7, 1789), "Prise de la Bastille");
+    Rdv e6(Date(9, 4, 2012), "reunion", Horaire(10, 45), Duree(1, 30), "etudiant", "passerelle");
+    Evt1jDur e7(Date(6, 12, 2012), "St Nicolas", Horaire(19, 0), Duree(45));
+    EvtPj e8(Date(21, 6, 2010), Date(28, 6, 2010), "examens");
+    Rdv e9(Date(19, 5, 2013), "rdv", Horaire(11, 45), Duree(2, 30), "Mr X", "Utseus");
     Agenda mon_agenda;
-    mon_agenda<<e1<<e2<<e3<<e4<<e5<<e6<<e7<<e8;
+    mon_agenda << e1 << e2 << e3 << e4 << e5 << e6 << e7 << e8;
 //iterator
-    for(auto it=mon_agenda.begin(); it!=mon_agenda.end(); ++it)
-        std::cout << *it <<"\n";
+    for (auto it = mon_agenda.begin(); it != mon_agenda.end(); ++it)
+        std::cout << *it << "\n";
 // const_iterator
-    for(auto it=mon_agenda.cbegin(); it!=mon_agenda.cend(); ++it)
-        std::cout  <<*it  <<"\n";
+    for (auto it = mon_agenda.cbegin(); it != mon_agenda.cend(); ++it)
+        std::cout << *it << "\n";
 // maintenant, on peut aussi utiliser un range for
-    for(auto& e : mon_agenda) std:cout<<e<<"\n";
+    for (auto &e : mon_agenda)
+        std:
+        cout << e << "\n";
 
 
     MyAgenda a;
-    for(MyAgenda::iterator it = a.begin(); it != a.end(); ++it )
+    for (MyAgenda::iterator it = a.begin(); it != a.end(); ++it)
         std::cout << *it << std::endl;
 
-    for(MyAgenda::iterator2 it = a.begin2(); it != a.end2(); ++it )
+    for (MyAgenda::iterator2 it = a.begin2(); it != a.end2(); ++it)
         std::cout << *it << std::endl;
 
     return 0;
