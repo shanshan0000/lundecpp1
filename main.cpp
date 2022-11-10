14083dc57bea956a30880914487b7139eeb2f130
Lunde Chen <lunde.chen@outlook.com>
2022-11-10 16:15:00 +0800

    cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include <set>
+#include <map>
+#include <list>
+
+
+class A : public std::set<int> {
+public:
+    void clear() {}
+};
+
+
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
