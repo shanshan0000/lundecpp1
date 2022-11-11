ade00725ff8bd18c4c0971562a967b89cb5d067e
Lunde Chen <lunde.chen@outlook.com>
2022-11-11 08:14:29 +0800

    cpp

--- graph.h
+++ graph.h
@@ -1,7 +1,3 @@
-//
-// Created by lunde on 2020/11/9.
-//
-
 #ifndef LO02_GRAPH_H
 #define LO02_GRAPH_H
 
@@ -15,50 +11,65 @@ using namespace std;
 class GraphException {
     string info;
 public:
-    GraphException(const string& str):info(str){}
+    GraphException(const string &str) : info(str) {}
+
     string getInfo() const { return info; }
 };
 
 template<class Vertex>
 class GraphG {
-    map<Vertex,set<Vertex> > adj;
+    map<Vertex, set<Vertex> > adj;
     unsigned int nbEdges;
     string name;
 public:
-    GraphG(const string& n):name(n),nbEdges(0){}
-    const string& getName() const { return name; }
-    unsigned int getNbVertices() const { return adj.size(); }
-    unsigned int getNbEdges() const { return nbEdges; }
-    void addVertex(const Vertex& i);
-    void addEdge(const Vertex& i, const Vertex& j);
-    void removeEdge(const Vertex& i, const Vertex& j);
-    void print(ostream& f) const;
+    GraphG(const string &n) : name(n), nbEdges(0) {}
 
-    class vertex_iterator : public map<Vertex,set<Vertex> >::const_iterator {
+    const string &getName() const { return name; }
+
+    unsigned int getNbVertices() const { return adj.size(); }
+
+    unsigned int getNbEdges() const { return nbEdges; }
+
+    void addVertex(const Vertex &i);
+
+    void addEdge(const Vertex &i, const Vertex &j);
+
+    void removeEdge(const Vertex &i, const Vertex &j);
+
+    void print(ostream &f) const;
+
+    class vertex_iterator : public map<Vertex, set<Vertex> >::const_iterator {
     public:
-        vertex_iterator():map<Vertex,set<Vertex> >::const_iterator(){}
-        vertex_iterator(typename map<Vertex,set<Vertex> >::const_iterator it):map<Vertex,
-                set<Vertex> >::const_iterator(it){}
-        const Vertex& operator*() const { return map<Vertex,set<Vertex> >::
-            const_iterator::operator*().first; }
+        vertex_iterator() : map<Vertex, set<Vertex> >::const_iterator() {}
+
+        vertex_iterator(typename map<Vertex, set<Vertex> >::const_iterator it) :
+                        map<Vertex, set<Vertex> >::const_iterator(it) {}
+
+        const Vertex &operator*() const {
+            return map<Vertex, set<Vertex> >::const_iterator::operator*().first;
+        }
     };
 
-    vertex_iterator begin_vertex() const { return vertex_iterator(adj.begin() ); }
+    vertex_iterator begin_vertex() const { return vertex_iterator(adj.begin()); }
+
     vertex_iterator end_vertex() const { return vertex_iterator(adj.end()); }
 
     class successor_iterator : public set<Vertex>::const_iterator {
     public:
-        successor_iterator():set<Vertex>::const_iterator(){}
-        successor_iterator(typename set<Vertex>::const_iterator it):set<Vertex>::const_iterator(it){}
+        successor_iterator() : set<Vertex>::const_iterator() {}
+
+        successor_iterator(typename set<Vertex>::const_iterator it) :
+                                    set<Vertex>::const_iterator(it) {}
     };
 
-    successor_iterator begin_successor(const Vertex& x) const;
-    successor_iterator end_successor(const Vertex& x) const;
+    successor_iterator begin_successor(const Vertex &x) const;
 
-
+    successor_iterator end_successor(const Vertex &x) const;
 
 };
-template<class V> void GraphG<V>::addVertex(const V& i){
+
+template<class V>
+void GraphG<V>::addVertex(const V &i) {
     adj[i];
 // OU
 // adj.insert(make_pair(i,set<V>()));
@@ -67,47 +78,52 @@ template<class V> void GraphG<V>::addVertex(const V& i){
 //if (it==adj.end()) adj[i]=set<V>();
 }
 
-template<class V> void GraphG<V>::addEdge(const V& i, const V& j){
+template<class V>
+void GraphG<V>::addEdge(const V &i, const V &j) {
     if (adj[i].insert(j).second)
         nbEdges++;
     adj[j];
 }
 
-template<class V> void GraphG<V>::removeEdge(const V& i, const V& j){
-    typename map<V,set<V> >::iterator it=adj.find(i);
-    if (it!=adj.end()) nbEdges-=it->second.erase(j);
+template<class V>
+void GraphG<V>::removeEdge(const V &i, const V &j) {
+    typename map<V, set<V> >::iterator it = adj.find(i);
+    if (it != adj.end()) nbEdges -= it->second.erase(j);
 }
 
 template<class V>
-void GraphG<V>::print(ostream& f) const {
-    f<<"Graph "<<getName()<<" ("<<getNbVertices()<<" vertices and "<<getNbEdges()<<"edges)"<<"\n";
-    for(typename map<V,set<V> >::const_iterator it=adj.begin(); it!=adj.end(); ++it){
-        f<<it->first<<":";
-        for(typename set<V>::const_iterator it2=it->second.begin(); it2!=it->second.end()
-                ; ++it2) f<<*it2<<" ";
-        f<<"\n";
+void GraphG<V>::print(ostream &f) const {
+    f << "Graph " << getName() << " (" << getNbVertices()
+            << " vertices and " << getNbEdges() << "edges)" << "\n";
+    for (typename map<V, set<V> >::const_iterator it = adj.begin(); it != adj.end(); ++it) {
+        f << it->first << ":";
+        for (typename set<V>::const_iterator it2 = it->second.begin();
+                    it2 != it->second.end(); ++it2)
+            f << *it2 << " ";
+        f << "\n";
     }
 }
 
 template<class V>
-ostream& operator<<(ostream& f, const GraphG<V>& G){
+ostream &operator<<(ostream &f, const GraphG<V> &G) {
     G.print(f);
     return f;
 }
 
 template<class V>
 class print {
-    const GraphG<V>& G;
-    ostream& flux;
+    const GraphG<V> &G;
+    ostream &flux;
 public:
-    print(ostream& f, const GraphG<V>& g):flux(f),G(g){}
-    void operator()(const V& x) {
-        flux<<x<<":";
-        for(typename GraphG<V>::successor_iterator it=G.begin_successor(x); it!=G.
-                end_successor(x); ++it){
-            flux<<*it<<" ";
+    print(ostream &f, const GraphG<V> &g) : flux(f), G(g) {}
+
+    void operator()(const V &x) {
+        flux << x << ":";
+        for (typename GraphG<V>::successor_iterator it = G.begin_successor(x); it != G.
+                end_successor(x); ++it) {
+            flux << *it << " ";
         }
-        flux<<"\n";
+        flux << "\n";
     }
 };
 
@@ -120,18 +136,17 @@ public:
 //}
 
 template<class V>
-typename GraphG<V>::successor_iterator GraphG<V>::begin_successor(const V& x) const
-{
-    typename map<V,set<V> >::const_iterator it=adj.find(x);
-    if (it!=adj.end()) return successor_iterator(it->second.begin());
+typename GraphG<V>::successor_iterator GraphG<V>::begin_successor(const V &x) const {
+    typename map<V, set<V> >::const_iterator it = adj.find(x);
+    if (it != adj.end()) return successor_iterator(it->second.begin());
     throw GraphException("bad argument: vertex not defined");
 }
 
 
 template<class V>
-typename GraphG<V>::successor_iterator GraphG<V>::end_successor(const V& x) const {
-    typename map<V,set<V> >::const_iterator it=adj.find(x);
-    if (it!=adj.end()) return successor_iterator(it->second.end());
+typename GraphG<V>::successor_iterator GraphG<V>::end_successor(const V &x) const {
+    typename map<V, set<V> >::const_iterator it = adj.find(x);
+    if (it != adj.end()) return successor_iterator(it->second.end());
     throw GraphException("bad argument: vertex not defined");
 }
 
--- main.cpp
+++ main.cpp
@@ -2,20 +2,30 @@
 #include "graph.h"
 
 int main() {
-    try{
+    try {
         GraphG<char> G2("G2");
-        G2.addVertex('a'); G2.addVertex('b'); G2.addEdge('a','c');
-        G2.addEdge('a','d'); G2.addEdge('d','e'); G2.addEdge('e','b');
-        cout<<G2;
-        cout<<"vertices of G2 are: ";
-        for(GraphG<char>::vertex_iterator it=G2.begin_vertex();
-            it!=G2.end_vertex(); ++it) cout<<*it<<" ";
-        cout<<"\nsuccessors of a: ";
-        for(GraphG<char>::successor_iterator it=G2.begin_successor('a');it!=G2.end_successor('a'); ++it){ std::cout<<*it<<" "; }
+        G2.addVertex('a');
+        G2.addVertex('b');
+        G2.addEdge('a', 'c');
+        G2.addEdge('a', 'd');
+        G2.addEdge('d', 'e');
+        G2.addEdge('e', 'b');
+        cout << G2;
+        cout << "vertices of G2 are: ";
+        for (GraphG<char>::vertex_iterator it = G2.begin_vertex();
+             it != G2.end_vertex(); ++it)
+            cout << *it << " ";
+        cout << "\nsuccessors of a: ";
+        for (GraphG<char>::successor_iterator it = G2.begin_successor('a'); it != G2.end_successor('a'); ++it) {
+            std::cout << *it << " ";
+        }
         GraphG<string> G3("Pref");
-        G3.addEdge("LO21","IA01"); G3.addEdge("IA02","IA01"); G3.addEdge("IA01","NF17");
-        G3.addEdge("IA02","NF16"); G3.addEdge("NF93","NF16");
-        cout<<G3;
-    }catch(exception e){ std::cout<<e.what()<<"\n"; }
+        G3.addEdge("LO21", "IA01");
+        G3.addEdge("IA02", "IA01");
+        G3.addEdge("IA01", "NF17");
+        G3.addEdge("IA02", "NF16");
+        G3.addEdge("NF93", "NF16");
+        cout << G3;
+    } catch (exception e) { std::cout << e.what() << "\n"; }
     return 0;
 }
