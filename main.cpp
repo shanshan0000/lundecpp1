083fc1a8cb30c9f0916d090d4c7543503b568767
Lunde Chen <lunde.chen@outlook.com>
2022-11-11 11:40:28 +0800

    v1

--- main.cpp
+++ main.cpp
@@ -5,15 +5,77 @@
 #include <list>
 
 
-class A : public std::set<int> {
+class FrequencySet : public std::list<char> {
 public:
-    void clear() {}
+    FrequencySet() : std::list<char>() {
+        counter = new std::map<char, int>();
+    }
+
+    ~FrequencySet() {
+        delete counter;
+    }
+
+    void insert(const char e) {
+        this->std::list<char>::push_back(e);
+        (*counter)[e] ++;
+    }
+
+    class iterator {
+    public:
+        iterator(std::map<char, int>::iterator it = std::map<char, int>::iterator())
+            : it_value(it) {}
+
+        iterator &operator++() {
+            ++it_value;
+            return *this;
+        }
+
+        bool operator==(const iterator& it) const {
+            return it_value == it.it_value;
+        }
+
+        bool operator!=(const iterator& it) const {
+            return it_value != it.it_value;
+        }
+
+        char operator*() const {
+            return it_value->first;
+        }
+
+    private:
+        std::map<char, int>::iterator it_value;
+    };
+
+    iterator begin() const {
+        return iterator(counter->begin());
+    }
+
+    iterator end() const {
+        return iterator(counter->end());
+    }
+
+private:
+    std::map<char, int>* counter;
 };
 
 
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FrequencySet s;
+    s.insert('C');
+    s.insert('B');
+    s.insert('B');
+    s.insert('B');
+    s.insert('B');
+    s.insert('A');
+    s.insert('B');
+    s.insert('B');
+    s.insert('B');
+    s.insert('C');
+    s.insert('C');
+    for(auto p : s) {
+        std::cout << p << " ";
+    }
     return 0;
 }
