07dd9570f57a31ec811aa1c3b8816d2dd580ef45
Lunde Chen <lunde.chen@outlook.com>
2022-11-11 12:14:13 +0800

    v3

--- main.cpp
+++ main.cpp
@@ -1,61 +1,75 @@
 #include <iostream>
 
 #include <set>
+#include <vector>
 #include <map>
 #include <list>
+#include <algorithm>
 
 
 class FrequencySet : public std::list<char> {
 public:
     FrequencySet() : std::list<char>() {
         counter = new std::map<char, int>();
+        v = new std::vector<char>();
     }
 
     ~FrequencySet() {
         delete counter;
+        delete v;
     }
 
     void insert(const char e) {
+        if(std::find(v->begin(), v->end(), e) == v->end()) {
+            v->push_back(e);
+        }
         this->std::list<char>::push_back(e);
         (*counter)[e] ++;
     }
 
     class iterator {
     public:
-        iterator(std::map<char, int>::iterator it = std::map<char, int>::iterator())
-            : it_value(it) {}
+        iterator(std::vector<char>::iterator it = std::vector<char>::iterator())
+            : it_freq(it) {}
 
         iterator &operator++() {
-            ++it_value;
+            ++it_freq;
             return *this;
         }
 
         bool operator==(const iterator& it) const {
-            return it_value == it.it_value;
+            return it_freq == it.it_freq;
         }
 
         bool operator!=(const iterator& it) const {
-            return it_value != it.it_value;
+            return it_freq != it.it_freq;
         }
 
         char operator*() const {
-            return it_value->first;
+            return *it_freq;
         }
 
     private:
-        std::map<char, int>::iterator it_value;
+        std::vector<char>::iterator it_freq;
     };
 
     iterator begin() const {
-        return iterator(counter->begin());
+        auto cam = [&](char a, char b) {
+        return (*counter)[a] == (*counter)[b] ? a > b : (*counter)[a] < (*counter)[b];
+        };
+        std::sort(v->begin(), v->end(), cam);
+        return iterator(v->begin());
     }
 
     iterator end() const {
-        return iterator(counter->end());
+        return iterator(v->end());
     }
+
+
 
 private:
     std::map<char, int>* counter;
+    std::vector<char>* v;
 };
 
 
