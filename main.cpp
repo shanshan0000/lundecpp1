43acc406e7b2add9fd7c0b11580f3516cb969947
Lunde Chen <lunde.chen@outlook.com>
2022-11-12 13:22:47 +0800

    cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#in#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class A {
+private:
+    int x;
+public:
+    class B {
+    private:
+        int x;
+    public:
+        void f() {
+            x=9;
+            cout << x << endl;
+        }
+    };
+
+    void f() {
+        x=99;
+        cout << x << endl;
+    }
+};
+
+int main()
+{
+    A a;
+    a.f();
+    A::B b;
+    b.f();
     return 0;
 }
