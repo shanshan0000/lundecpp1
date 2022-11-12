413dddd422848b62b152206b432835b9ad369103
Lunde Chen <lunde.chen@outlook.com>
2022-11-12 14:03:15 +0800

    cpp

--- timing.h
+++ timing.h
@@ -4,19 +4,16 @@
 #include<string>
 #include<cstring>
 
-using  namespace  std;
+using namespace std;
 
-namespace TIME
-{
-    class Date
-    {
+namespace TIME {
+    class Date {
     public:
         int year, month, day;
-        Date(int y, int m, int d): year(y), month(m), day(d)
-        {}
 
-        string toString() const
-        {
+        Date(int y, int m, int d) : year(y), month(m), day(d) {}
+
+        string toString() const {
             return std::to_string(year)
                    + " "
                    + std::to_string(month)
@@ -25,34 +22,33 @@ namespace TIME
         }
     };
 
-    class Duree
-    {
+    class Duree {
     public:
         int duree;
-        Duree(int d): duree(d){}
-        Duree(int _d, int d): duree(d){}
 
-        string toString() const
-        {
+        Duree(int d) : duree(d) {}
+
+        Duree(int _d, int d) : duree(d) {}
+
+        string toString() const {
             return std::to_string(duree);
         }
     };
 
-    class Horaire
-    {
+    class Horaire {
     public:
         int hour, minute;
-        Horaire(int h, int m):hour(h), minute(m){}
 
-        string toString() const
-        {
+        Horaire(int h, int m) : hour(h), minute(m) {}
+
+        string toString() const {
             return std::to_string(hour)
                    + " "
                    + std::to_string(minute);
         }
     };
 
-    inline std::ostream & operator<<(std::ostream & F, const Date & d) {
+    inline std::ostream &operator<<(std::ostream &F, const Date &d) {
         string s = std::to_string(d.year)
                    + " "
                    + std::to_string(d.month)
@@ -62,20 +58,19 @@ namespace TIME
         return F;
     }
 
-    inline std::ostream & operator<<(std::ostream & F, const Duree & d) {
+    inline std::ostream &operator<<(std::ostream &F, const Duree &d) {
         string s = std::to_string(d.duree);
         F << s;
         return F;
     }
 
-    inline std::ostream & operator<<(std::ostream & F, const Horaire & h) {
+    inline std::ostream &operator<<(std::ostream &F, const Horaire &h) {
         string s = std::to_string(h.hour)
                    + " "
                    + std::to_string(h.minute);
         F << s;
         return F;
     }
-
 }
 
 
