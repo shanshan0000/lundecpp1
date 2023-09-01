21b519054b3fc8a9d81bc2a8243391581f62fe57
Lunde Chen <lunde.chen@outlook.com>
2023-09-01 12:24:40 +0800

    a

--- saturday.cpp
+++ saturday.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "fonction.h"
 
 int fct(int x, float y){ std::cout<<"3:"<<x<<y<<"\n"; return 0; }
 float fct(float x, int y){ std::cout<<"4:"<<x<<y<<"\n"; return 3.14; }
